c7de8bc04656eb125660a37840f95a4864ec875d
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-03-31 16:47:27 -0300

    Delete LojaComBanco directory

--- LojaComBanco/main.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	require_once 'Produtos/Produto.php';
-	require_once 'Produtos/Camiseta.php';
-	require_once 'Produtos/Shorts.php';
-    require_once 'Produtos/Tenis.php';
-	require_once 'Produtos/Conexao.php';
-
-	Conexao::abreConexao();
-?>
