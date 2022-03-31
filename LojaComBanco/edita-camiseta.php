b8e1c0a0a72d11bd618bc93e6a8b87b97d020eb2
Laurentino <55161637+matheus-laurentino29@users.noreply.github.com>
2022-03-31 14:41:47 -0300

    Add files via upload

--- LojaComBanco/edita-camiseta.php
+++ LojaComBanco/edita-camiseta.php
@@ -1,10 +1,8 @@
 <?php
-include("Produtos/Conexao.php");
+    include("Produtos/Conexao.php");
 
-
-$idCamiseta = $_GET["idCamiseta"];
-$sqlRegistros = mysqli_query($conexao,"select tipo,marca,modelo,tamanho,cor,data_lancamento,ocasiao,manga,estampa,quantidade from bdcamiseta") or die ("Erro na execusão do contato" . mysqli_error($conexao));
-
+    $idCamiseta = $_GET["idCamiseta"];
+    $sqlRegistros = mysqli_query($conexao,"select tipo,marca,modelo,tamanho,cor,data_lancamento,ocasiao,manga,estampa,quantidade from bdcamiseta") or die ("Erro na execusão do contato" . mysqli_error($conexao));
 
     $dados = mysqli_fetch_array($sqlRegistros);
     $tipo = $dados["tipo"];
@@ -18,76 +16,185 @@ $sqlRegistros = mysqli_query($conexao,"select tipo,marca,modelo,tamanho,cor,data
     $estampa = $dados["estampa"];
     $quantidade = $dados["quantidade"];
 
-    // ------------------
-
-
-
-   
 ?>
 
 <!doctype html>
+
 <html>
-<head><meta charset="utf-8">
-<title>Editar Produto</title>
+
+<head>
+
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="estilizacao.css" media="screen" />
+    <title>Editar Produto</title>
+
 </head>
-<body>
-<form name="edCamiseta" method="post" action="editaC.php" align="center">
 
-    <input type="hidden" name="idCamiseta" value="<?php echo $idCamiseta;?>"> 
+<body class="bg-white">
 
-         <?php
-        $rCamiseta = mysqli_query($conexao,"select * from bdCamiseta WHERE idCamiseta = $idCamiseta");
-        $linhas = mysqli_num_rows($rCamiseta);
-        for($i=0;$i<$linhas;$i++){
-        $dados = mysqli_fetch_array($rCamiseta);
-        $tipo = $dados["tipo"];
-        $marca = $dados["marca"];
-        $modelo = $dados["modelo"];
-        $tamanho = $dados["tamanho"];
-        $cor = $dados["cor"];
-        $data_lancamento = $dados["data_lancamento"];
-        $ocasiao = $dados["ocasiao"];
-        $manga = $dados["manga"];
-        $estampa = $dados["estampa"];
-        $quantidade = $dados["quantidade"];
-        }
+    <nav class="site-header sticky-top py-1">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2 d-none d-md-inline-block" href="#"> </a>
+            <a class="py-2" href="index.php">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="WHITE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+            </a>
+            <a class="py-2 d-none d-md-inline-block" href="#"> </a>
+        </div>
+    </nav>
+
+    <div align="center">
+
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-5 font-weight-normal">LauJohns Enterprise</h1>
+            <img class="d-block mx-auto mb-4" src="Imagens/edita.png" alt="" width="72"height="72">
+            <p class="lead font-weight-normal"> Faça a edição de seu produto.</p>
+        </div>
+
+        <!-- <div class="product-device shadow-sm d-none d-md-block"></div> -->
+        <!-- <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div> -->
+   
+    </div>
+
+    <form name="edCamiseta" method="post" action="editaC.php" align="center">
+
+        <input type="hidden" name="idCamiseta" value="<?php echo $idCamiseta;?>"> 
+
+        <?php
+
+            $rCamiseta = mysqli_query($conexao,"select * from bdCamiseta WHERE idCamiseta = $idCamiseta");
+            $linhas = mysqli_num_rows($rCamiseta);
+
+            for($i=0;$i<$linhas;$i++){
+                $dados = mysqli_fetch_array($rCamiseta);
+                $tipo = $dados["tipo"];
+                $marca = $dados["marca"];
+                $modelo = $dados["modelo"];
+                $tamanho = $dados["tamanho"];
+                $cor = $dados["cor"];
+                $data_lancamento = $dados["data_lancamento"];
+                $ocasiao = $dados["ocasiao"];
+                $manga = $dados["manga"];
+                $estampa = $dados["estampa"];
+                $quantidade = $dados["quantidade"];
+            }
+
         ?>
+        
+        <div class="col-md-12 order-md-1">
 
-    <form class=edita action="editaC.php" method="post">
+            <form class=edita action="editaC.php" method="post">
 
-        <label> marca:</label>
-        <input type="text" name="marca" value="<?php echo $marca; ?>">
-        <br>
-    
-        <label> modelo:</label>
-        <input type="text" name="modelo" value="<?php echo $modelo; ?>">
-        <br>
-        <label> tamanho:</label>
-        <input type="text" name="tamanho" value="<?php echo $tamanho; ?>">
-        <br>
-        <label> cor:</label>
-        <input type="text" name="cor" value="<?php echo $cor; ?>">
-        <br>
-        <label> data_lancamento:</label>
-        <input type="date" name="data_lancamento" value="<?php echo $data_lancamento; ?>">
-        <br>
-        <label> ocasiao:</label>
-        <input type="text" name="ocasiao" value="<?php echo $ocasiao; ?>">
-        <br>
-        <label> manga:</label>
-        <input type="text" name="manga" value="<?php echo $manga; ?>">
-        <br>
-        <label> estampa:</label>
-        <input type="text" name="estampa" value="<?php echo $estampa; ?>">
-        <br>
-        <label> quantidade:</label>
-        <input type="text" name="quantidade" value="<?php echo $quantidade; ?>">
+                                    <!--Primeira coluna-->
 
- 
+                <div class="row">
 
+                    <div class="col-md-6 mb-3">
+                        <input type="hidden" name="tipo" value="c">
+                        <label class="font-weight-bold" for="modelo">Modelo</label>
+                        <input type="text" class="form-control" id="modelo" name="modelo" value="<?php echo $modelo; ?>">
+                    </div>
 
-        <input type="submit" name="atualizar-camiseta" id="atualizar-camiseta" value="Atualizar">
+                    <div class="col-md-6 mb-3">
+                        <label class="font-weight-bold" for="tamanho">Tamanho</label>
+                        <select class="custom-select d-block w-100" id="tamanho" name="tamanho" required="">
+                            <option value="<?php echo $tamanho;?>"><?php echo $tamanho;?></option>
+                            <option value="PP">PP</option>
+                            <option value="P">P</option>
+                            <option value="M">M</option>
+                            <option value="G">G</option>
+                            <option value="GG">GG</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                                    <!--Segunda coluna-->
+
+                <div class="row">
+
+                    <div class="col-md-6 mb-3">
+                        <label class="font-weight-bold" for="cor">Cor</label>
+                        <input type="text" class="form-control" id="cor" name="cor" value="<?php echo $cor; ?>">
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label class="font-weight-bold" for="marca">Marca</label>
+                        <select class="custom-select d-block w-100" id="marca" name="marca" required="">
+                            <option value="<?php echo $marca; ?>"><?php echo $marca; ?></option>
+                            <option value="Nike">Nike</option>
+                            <option value="Adidas">Adidas</option>
+                            <option value="Puma">Puma</option>
+                            <option value="Lacoste">Lacoste</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                                    <!--Terceira coluna-->
+                
+                <div class="row">
+
+                    <div class="col-md-6 mb-3">
+                        <label class="font-weight-bold" for="manga">Manga</label>
+                        <select class="custom-select d-block w-100" id="manga" name="manga" required="">
+                            <option value="<?php echo $manga; ?>"><?php echo $manga; ?></option>
+                            <option value="s">Sim</option>
+                            <option value="n">Não</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label class="font-weight-bold" for="estampa">Estampa</label>
+                        <select class="custom-select d-block w-100" id="estampa" name="estampa" required="">
+                            <option value="<?php echo $estampa; ?>"><?php echo $estampa; ?></option>
+                            <option value="s">Sim</option>
+                            <option value="n">Não</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                                    <!--Quarta coluna-->
+
+                <div class="row">
+
+                    <div class="col-md-6 mb-3" >
+                        <label class="font-weight-bold" for="data_lancamento">Data de lançamento</label>
+                        <input type="date" class="form-control" id="data_lancamento" name="data_lancamento" value="<?php echo $data_lancamento; ?>">
+                    </div>
+
+                    <div class="col-md-6 mb-3">
+                        <label class="font-weight-bold" for="ocasiao">Ocasião</label>
+                        <input type="text" class="form-control" id="ocasiao" name="ocasiao" value="<?php echo $ocasiao; ?>">
+                    </div>
+
+                </div>
+
+                                    <!--Quinta coluna-->
+
+                <div class="row">
+
+                    <div class="col-md-6 mb-3" >
+                        <label class="font-weight-bold" for="quantidade">Quantidade de itens</label>
+                        <input type="int" class="form-control" id="quantidade" name="quantidade" value="<?php echo $quantidade; ?>">
+                    </div>
+
+                </div>
+            
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Atualizar</button>
+
+                <!----------------------------------------------------------------------------------------------->
+
+            </form>
+
+        </div>
 
     </form>
+
+    <footer class="my-5 pt-5 text-muted text-center text-small">
+        <p class="mb-1"> LauJohns Enterprise</p>
+    </footer>
+
 </body>
 </html>
--- LojaComBanco/index.php
+++ LojaComBanco/index.php
@@ -5,30 +5,25 @@
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">    <title>Página Inicial</title>
     <link rel="stylesheet" type="text/css" href="estilizacao.css" media="screen" />
+
     
 </head>
 
 <body> 
     <nav class="site-header sticky-top py-1">
         <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-2 d-none d-md-inline-block" href="#"> </a>
             <a class="py-2" href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="WHITE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mx-auto"><circle cx="12" cy="12" r="10"></circle><line x1="14.31" y1="8" x2="20.05" y2="17.94"></line><line x1="9.69" y1="8" x2="21.17" y2="8"></line><line x1="7.38" y1="12" x2="13.12" y2="2.06"></line><line x1="9.69" y1="16" x2="3.95" y2="6.06"></line><line x1="14.31" y1="16" x2="2.83" y2="16"></line><line x1="16.62" y1="12" x2="10.88" y2="21.94"></line></svg>
             </a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Tour</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Produto</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Features</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Premium</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Suporte</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Preços</a>
-            <a class="py-2 d-none d-md-inline-block" href="#">Carrinho</a>
+            <a class="py-2 d-none d-md-inline-block" href="#"> </a>
         </div>
     </nav>
 
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-white">
       <div class="col-md-5 p-lg-5 mx-auto my-5">
         <h1 class="display-5 font-weight-normal">LauJohns Enterprise</h1>
-        <p class="lead font-weight-normal">E uma descrição engraçadinha, para continuar. Alavanque seus esforços em marketing com esse exemplo, baseado nas páginas de marketing da Apple.</p>
-        <a class="btn btn-outline-secondary" href="#">Em breve</a>
+        <p class="lead font-weight-normal">O site com banco de dados perfeito para ser usado em sua loja com a finalidade de gerenciar os produtos dela.</p>
       </div>
       <div class="product-device shadow-sm d-none d-md-block"></div>
       <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
@@ -36,28 +31,29 @@
         <div class="container">
             <div class="jumbotron">
             </div>
+            <hr size="5">
+
                 <br>
                 <div class="row">
                     <br>
-                    <h3>Camisetas</h3>
-                    <p>
-                        <a href="cadastroCamiseta.html" class="btn btn-success">Adicionar</a>
-                    </p>
+                    <h3 align="center">Estoque de Camisetas</h3>
+                        <br>
+                        <br>
+                        <br>
                     <table class="table table-bordered">
                         <thead class="table-secondary">
                             <tr class="border border-4">
                                 <th scope="col"style="text-align:center">Id</th >
-                                <th scope="col"style="text-align:center">marca</th>
-                                <th scope="col" style="text-align:center">modelo</th>
-                                <th scope="col"style="text-align:center">tamanho</th>
-                                <th scope="col"style="text-align:center">cor</th>
-                                <th scope="col"style="text-align:center">data_lancamento</th>
-                                <th scope="col"style="text-align:center">ocasiao</th>
-                                <th scope="col"style="text-align:center">manga</th>
-                                <th scope="col"style="text-align:center">estampa</th>
-                                <th scope="col"style="text-align:center">quantidade</th>
-                                <th scope="col" style="text-align:center">Opções</th>
-                                                          
+                                <th scope="col"style="text-align:center">Marca</th>
+                                <th scope="col" style="text-align:center">Modelo</th>
+                                <th scope="col"style="text-align:center">Tamanho</th>
+                                <th scope="col"style="text-align:center">Cor</th>
+                                <th scope="col"style="text-align:center">Data de fabricação</th>
+                                <th scope="col"style="text-align:center">Ocasiao</th>
+                                <th scope="col"style="text-align:center">Manga</th>
+                                <th scope="col"style="text-align:center">Estampa</th>
+                                <th scope="col"style="text-align:center">Quantidade</th>
+                                <th scope="col" style="text-align:center">Opções</th>            
                             </tr>
                         </thead>
                         <tbody>
@@ -81,8 +77,8 @@
                                 
 
                                 echo '<td width=250>';
-                                echo '<p align="center"> <a class="btn btn-danger" href="excluircamiseta.php?idCamiseta='.$row['idCamiseta'].'" >Excluir</a>' . "   " 
-                                . '<a class="btn btn-warning" href="edita-camiseta.php?idCamiseta='.$row['idCamiseta'].'">Editar</a> </p>';
+                                echo '<p align="center"> <a class="button button3" href="excluircamiseta.php?idCamiseta='.$row['idCamiseta'].'" >Excluir</a>' . "   " 
+                                . '<a class="button button2" href="edita-camiseta.php?idCamiseta='.$row['idCamiseta'].'">Editar</a> </p>';
                                 echo '</td>';
                                 echo '</tr>';
                             }
@@ -90,26 +86,32 @@
                             ?>
                         </tbody>
                     </table>
+                    <p align="center"> 
+                    <a href="cadastroCamiseta.html" class="button button1"> Adiconar ao estoque</a>
+                    </p>
+                    
                 </div>
+
+                <hr size="5">
 
                 <div class="row">
                     <br>
-                    <h3>Shorts</h3>
-                    <p>
-                        <a href="cadastroShorts.html" class="btn btn-success">Adicionar</a>
-                    </p>
-                    <table class="table">
+                    <h3 align="center">Estoque de Shorts</h3>
+                        <br>
+                        <br>
+                        <br>
+                    <table class="table table-bordered">
                     <thead class = "table-secondary">
                             <tr class="border border-4">
-                                <th scope="col"style="text-align:center">Id</th>
-                                <th scope="col"style="text-align:center">marca</th>
-                                <th scope="col"style="text-align:center">modelo</th>
-                                <th scope="col"style="text-align:center">tamanho</th>
-                                <th scope="col"style="text-align:center">cor</th>
-                                <th scope="col"style="text-align:center">data_lancamento</th>
-                                <th scope="col"style="text-align:center">ocasiao</th>
-                                <th scope="col"style="text-align:center">quantidade</th>
-                                <th scope="col" style="text-align:center">Opções</th>
+                            <th scope="col"style="text-align:center">Id</th >
+                                <th scope="col"style="text-align:center">Marca</th>
+                                <th scope="col" style="text-align:center">Modelo</th>
+                                <th scope="col"style="text-align:center">Tamanho</th>
+                                <th scope="col"style="text-align:center">Cor</th>
+                                <th scope="col"style="text-align:center">Data de fabricação</th>
+                                <th scope="col"style="text-align:center">Ocasiao</th>
+                                <th scope="col"style="text-align:center">Quantidade</th>
+                                <th scope="col" style="text-align:center">Opções</th>    
 
                             </tr>
                         </thead>
@@ -131,8 +133,8 @@
                                 echo '<td>'. $row['quantidade'] . '</td>';
 
                                 echo '<td width=250>';
-                                echo ' <p align="center"> <a class="btn btn-danger" href="excluirshort.php?idShort='.$row['idShort'].'">Excluir</a>' . '     
-                                . <a class="btn btn-warning" href="edita-shorts.php?idShort='.$row['idShort'].'">Editar</a> </p>';
+                                echo ' <p align="center"> <a class="button button3" href="excluirshort.php?idShort='.$row['idShort'].'">Excluir</a>' . '     
+                                    <a class="button button2" href="edita-shorts.php?idShort='.$row['idShort'].'">Editar</a> </p>';
                                 echo '</td>';
                                 echo '</tr>';
                             }
@@ -141,29 +143,33 @@
                         </tbody>
                         
                     </table>
-                        <br>
-                            <br>
+                    <p align="center"> 
+                    <a href="cadastroCamiseta.html" class="button button1"> Adiconar ao estoque</a>
+                    </p>
+                    <br>
+                    <br>
                 </div>
+                <hr size="5">
 
                 <div class="row">
                     <br>
-                    <h3>Tênis</h3>
-                    <p>
-                        <a href="cadastroTenis.html" class="btn btn-success">Adicionar</a>
-                    </p>
-                    <table class="table">
+                    <h3 align="center">Estoque de Tênis</h3>
+                        <br>
+                        <br>
+                        <br>
+                    <table class="table table-bordered">
                     <thead class="table-secondary">
                             <tr class="border border-4">
-                                <th scope="col"style="text-align:center">Id</th>
-                                <th scope="col"style="text-align:center">marca</th>
-                                <th scope="col"style="text-align:center">modelo</th>
-                                <th scope="col"style="text-align:center">tamanho</th>
-                                <th scope="col"style="text-align:center">cor</th>
-                                <th scope="col"style="text-align:center">lancamento</th>
-                                <th scope="col"style="text-align:center">ocasiao</th>
-                                <th scope="col"style="text-align:center">cadarço</th>
-                                <th scope="col" style="text-align:center">quantidade</th>
-                                <th scope="col" style="text-align:center">Opções</th>
+                            <th scope="col"style="text-align:center">Id</th >
+                                <th scope="col"style="text-align:center">Marca</th>
+                                <th scope="col" style="text-align:center">Modelo</th>
+                                <th scope="col"style="text-align:center">Tamanho</th>
+                                <th scope="col"style="text-align:center">Cor</th>
+                                <th scope="col"style="text-align:center">Data de fabricação</th>
+                                <th scope="col"style="text-align:center">Ocasiao</th>
+                                <th scope="col"style="text-align:center">Cadarço</th>
+                                <th scope="col"style="text-align:center">Quantidade</th>
+                                <th scope="col" style="text-align:center">Opções</th>    
 
                             </tr>
                         </thead>
@@ -187,8 +193,8 @@
                                 echo '<td>'. $row['quantidade'] . '</td>';
 
                                 echo '<td width=250>';
-                                echo '<p align="center"> <a class="btn btn-danger" href="excluirtenis.php?idTenis='.$row['idTenis'].'">Excluir</a>' . '     
-                                . <a class="btn btn-warning" href="edita-tenis.php?idTenis='.$row['idTenis'].'">Editar</a>';
+                                echo '<p align="center"> <a class="button button3" href="excluirtenis.php?idTenis='.$row['idTenis'].'">Excluir</a>' . '     
+                                <a class="button button2" href="edita-tenis.php?idTenis='.$row['idTenis'].'">Editar</a>';
                                 echo '</td>';
                                 echo '</tr>';
                             }
@@ -197,9 +203,14 @@
                             
                         </tbody>
                     </table>
+                   
+                    <p align="center"> 
+                    <a href="cadastroTenis.html" class="button button1"> Adiconar ao estoque</a>
+                    </p>
                 </div>
                             
-                 
+                <hr size="5">
+
 
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
